f031fcfe599dc1ab9917ff8e20df1498bc1980ef
Cole Miller <cole.t.miller@gmail.com>
2019-10-09 16:37:44 -0500

    finished html and css

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+var question1 = $(".q1").val();
+var question2 = $(".q2").val();
+var question3 = $(".q3").val();
+var question4 = $(".q4").val();
+var question5 = $(".q5").val();
+
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+
+
+
+
+$("#button").on("click", function () {
+    console.log("submit");
+
+
+});
